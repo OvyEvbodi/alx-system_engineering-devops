d9a530d4abace191371d75050bcee9f9e71a7e55
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-31 14:24:58 +0100

    use print instead of puts, for formatting

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:)([A-Z]+)?([a-z]+)?(\+?[0-9]+)?/).join
-puts ARGV[0].scan(/(?<=to:)([A-Z]+)?([a-z]+)?(\+?[0-9]+)?/).join
-puts ARGV[0].scan(/(?<=flags:)([0-9\-:]+)?/).join
+print ARGV[0].scan(/(?<=from:)([A-Z]+)?([a-z]+)?(\+?[0-9]+)?/).join
+print (",")
+print ARGV[0].scan(/(?<=to:)([A-Z]+)?([a-z]+)?(\+?[0-9]+)?/).join
+print (",")
+print ARGV[0].scan(/(?<=flags:)([0-9\-:]+)?/).join
