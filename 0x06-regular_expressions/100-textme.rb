413346579b5ce459aeb7bba0e3f2999d02fb8cb5
Ovy-Evbodi <evbodiovo@gmail.com>
2023-01-31 14:19:42 +0100

    regex backreferencing

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
 puts ARGV[0].scan(/(?<=from:)([A-Z]+)?([a-z]+)?(\+?[0-9]+)?/).join
+puts ARGV[0].scan(/(?<=to:)([A-Z]+)?([a-z]+)?(\+?[0-9]+)?/).join
+puts ARGV[0].scan(/(?<=flags:)([0-9\-:]+)?/).join
