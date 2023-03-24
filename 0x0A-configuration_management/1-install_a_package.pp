32dc14c48169181173429be30dce1f8a022196de
Ovy Evbodi <evbodiovo@gmail.com>
2023-03-24 14:46:40 +0000

    fix: add provider attribute instead of installing it as a dependency

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # Installs flask from pip3
 
-exec { "pip3":
-  command => "sudo apt-get install pip3"
-}
-
 package { "flask":
   ensure => "2.1.0",
-  require => Exec["pip3"]
+  provider => "pip3"
 }
