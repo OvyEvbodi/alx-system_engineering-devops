aad4929a8b7b079f920c2d4f8cf1b5229d2e1784
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-27 00:56:14 +0100

    Feat: Add puppet configuration

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# configure a server
+
+file_line {'no_password_authentication':
+  ensure => present,
+  path   => '/etc/ssh/sshd_config'
+  line   => 'IdentityFile ~/.ssh/school'
+}
+
+file_line { 'school_identity_file':
+  ensure => present,
+  path   => '/etc/ssh/sshd_config'
+  line   => 'PasswordAuthentication no'
+}
