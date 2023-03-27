ac8a1d9db336c76322aa8568035cf863f3ad48dc
Ovy-Evbodi <evbodiovo@gmail.com>
2023-03-27 01:23:08 +0100

    Use exec

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,5 @@
 # configure a server
 
-file_line {'no_password_authentication':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config'
-  line   => '  IdentityFile ~/.ssh/school'
-}
-
-file_line { 'school_identity_file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config'
-  line   => '  PasswordAuthentication no'
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
 }
