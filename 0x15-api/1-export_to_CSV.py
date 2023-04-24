a7c1ac0b3b548e4b4788dc1367366c33d0edd088
Ovy Evbodi <evbodiovo@gmail.com>
2023-04-24 19:26:05 +0100

    Export data in csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""Queries a REST API to get information on employees' todos"""
+
+from requests import get
+from sys import argv
+import csv
+
+
+def getTodos():
+    """Gets an employee's todo data using an API"""
+
+    if len(argv) > 1:
+        user_id = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        user_url = "{}users/{}".format(url, user_id)
+
+        response_user = get(user_url)
+        username = response_user.json().get("username")
+
+        todo_url = "{}todos?userId={}".format(url, user_id)
+
+        response_todo = get(todo_url).json()
+
+        path = "{}.csv".format(user_id)
+        with open(path, 'w', newline='') as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            for todo in response_todo:
+                writer.writerow([user_id, username, todo.get("completed"),
+                                todo.get("title")])
+        
+
+if __name__ == '__main__':
+    getTodos()
